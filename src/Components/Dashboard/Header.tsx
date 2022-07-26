d2239c6d81c99853e5fe798a844529333618a44f
unknown <manikandan.jn@fibonalabs.com>
2022-07-26 20:21:37 +0530

    Search over

---
+++ src/Components/Dashboard/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Layout, Row, Col } from "antd";
+import SearchBar from './SearchBar';
+import CreateWorkflow from './CreateWorkflow';
+//import cardDetails from '../Content/CardData';
+
+const { Header } = Layout;
+
+
+// function HeaderPart({refresh}) {
+
+// HeaderPart Main function 
+  export default ({refresh, searchCardCallBack}:any) => {
+
+      return (
+        <Header>
+            <Row>
+            <Col span={6}>
+                <p className="lefthead">Workflow</p>
+            </Col>
+                <SearchBar searchCardCallBack={searchCardCallBack} />
+                <CreateWorkflow refresh={refresh} />
+            </Row>
+      </Header>
+      )
+}
+
+//export default HeaderPart;
